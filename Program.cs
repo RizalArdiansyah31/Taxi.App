5de5ca34e0c2d5cbd9dd4df13a9dfc2f20ece5eb
Rizal Ardiansyah <101812798+RizalArdiansyah31@users.noreply.github.com>
2022-03-23 21:58:58 +0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rizal_Ardiansyah
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // membuat object taxi
+            Taxi taxi = new Taxi();
+
+            //seting parameter taxi
+            taxi.DriverName = "Rizal Ardiansyah";
+            taxi.OnDuty = true;
+            taxi.NumPassenger = 5;
+
+            taxi.TaxiInfo();
+            taxi.PickUpPassenger();
+            taxi.DropOffPassenger();
+
+            Console.ReadKey();
+
+        }
+    }
+}
